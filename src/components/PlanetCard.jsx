e44b570bf941cea1e252451bd748141b193d1e84
Manish Sameer <sameer@Manishs-MacBook-Air.local>
2024-02-24 16:21:35 +0530

    update planetcard

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -31,7 +31,7 @@ function PlanetCard({residentUrl}) {
       <div className="border col-span-6 md:col-span-3 lg:col-span-2 border-gray-300 rounded p-2">
         <p className="font-bold">Name: {resident.name}</p>
         <p>Gender: {resident.gender}</p>
-        <p>Mass: {resident.mass}</p>
+        <p>Mass: {resident.mass} kg</p>
         <p>Height: {resident.height} m</p>
       </div>
     </>
